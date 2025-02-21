470af877c8c3af3ba4881ecf9f2bd8fbd5e86c78
YufanPhomboLimbu <83822630+yufanphombolimbu@users.noreply.github.com>
2025-02-21 14:05:41 +0545

    Update Audit-mode.ps1
    updating script

--- Audit-mode.ps1
+++ Audit-mode.ps1
@@ -1,10 +1,10 @@
 #Powershell script extracting log from event viewer.
 $EventIDs = @(3099, 3085, 3102, 3105, 3096, 3101, 3108, 3089, 3076)
 $LogName = "Microsoft-Windows-CodeIntegrity/Operational"
-$OutputFile = "C:\Users\developer-pc\Documents\script\CodeIntegrityLogs.csv"
-$outputfilexml = "C:\Users\developer-pc\Documents\script\CodeIntegrityLogsxml.csv"
-$outfilexml = "C:\Users\developer-pc\Documents\script\CodeIntegrityLogsxml.csv"
-# Get event details
+$OutputFile = "C:\CodeIntegrityLogs.csv"
+$outputfilexml = "C:\CodeIntegrityLogsxml.csv"
+$outfilexml = "C\CodeIntegrityLogsxml.csv"
+# Get event details maximun 100 
 $Events = Get-WinEvent -LogName $LogName -Maxevent 100 | Where-Object { $_.Id -in $EventIDs }
 
 # Process and export events
@@ -31,7 +31,7 @@ $Events | ForEach-Object {
 
 # Save to CSV
 $Events | Select-Object TimeCreated, Id, ProviderName, LevelDisplayName, Message | Out-File -Encoding utf8 $OutputFile
- Export-Csv -Path "C:\Users\developer-pc\Documents\script\CodeIntegrityLogs.csv" -NoTypeInformation
+ Export-Csv -Path "C:\CodeIntegrityLogs.csv" -NoTypeInformation
 
 Write-Output "Events saved to $OutputFile"
 Write-Output "Events saved to $outputfilexml"
